23a08585b0615dc03fdf68c1ed55db0b89570460
b1015136 <b1015136@gmail.com>
2020-08-25 13:00:42 +0000

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_124647) do
+ActiveRecord::Schema.define(version: 2020_08_25_125731) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "penname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "name"

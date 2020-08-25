75f3e1bc667c087de7ab67ad85f15782ad2b22b1
b1015136 <b1015136@gmail.com>
2020-08-25 13:23:14 +0000

    bundle exec annotate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id           :integer          not null, primary key
+#  name         :string
+#  price        :integer
+#  published_on :date
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  publisher_id :integer          not null
+#
+# Indexes
+#
+#  index_books_on_publisher_id  (publisher_id)
+#
+# Foreign Keys
+#
+#  publisher_id  (publisher_id => publishers.id)
+#
 class Book < ApplicationRecord
   COSTLY_PRICE = 3000
   scope :costly, lambda { where("price > ?", COSTLY_PRICE) }

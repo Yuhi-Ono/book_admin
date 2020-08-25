79f72b82d694e461a9340f2e196fde203f996aef
b1015136 <b1015136@gmail.com>
2020-08-25 12:40:51 +0000

    scope を追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  COSTLY_PRICE = 3000
+  scope :costly, lambda { where("price > ?", COSTLY_PRICE) }
+  scope :written_about, lambda {|theme|
+    where("name like ?", "%#{theme}%")
+  }
 end
